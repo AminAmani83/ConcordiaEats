96a56b24c47e85635995f6b3be7c3673ce7b4d90
Xiao Vicky <WXIA00907012@maax.com>
2023-03-05 00:55:19 -0500

    CWA-125 implement product DAO and Service to fetch all products

--- JtProject/src/main/java/ca/concordia/eats/dao/ProductDaoImpl.java
+++ JtProject/src/main/java/ca/concordia/eats/dao/ProductDaoImpl.java
@@ -2,15 +2,37 @@ package ca.concordia.eats.dao;
 
 import ca.concordia.eats.dto.Category;
 import ca.concordia.eats.dto.Product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public class ProductDaoImpl implements ProductDao {
+	
+	@Autowired
+    private JdbcTemplate jdbcTemplate;
+	
     @Override
     public List<Product> fetchAllProducts() {
-        return null;
+    	return jdbcTemplate.query(
+                "select p.id, p.name, p.description, p.imagePath, p.price, p.salesCount, p.isOnSale, p.discountPercent, r.rating, c.id as categoryId, c.name as categoryName from product p join category c on p.categoryid = c.id join rating r on r.productId = p.id",
+                (rs, rowNum) ->
+                        new Product(
+                                rs.getInt("id"),
+                                rs.getString("name"),
+                                rs.getString("description"),
+                                rs.getString("imagePath"),
+                                rs.getFloat("price"),
+                                rs.getInt("salesCount"),
+                                rs.getBoolean("isOnSale"),
+                                rs.getFloat("discountPercent"),
+                                rs.getDouble("rating"),
+                                new Category(rs.getInt("categoryId"), rs.getString("categoryName"))
+                        )
+        );
     }
 
     @Override
