3e98bda3ab259611d4b95648c3aab45bbb1ac9f1
OleksandrArt <124222926+OleksandrArt@users.noreply.github.com>
2023-03-06 14:55:31 -0500

    Update User.java

--- JtProject/src/main/java/ca/concordia/eats/dto/User.java
+++ JtProject/src/main/java/ca/concordia/eats/dto/User.java
@@ -1,8 +1,40 @@
 package ca.concordia.eats.dto;
 
 public class User {
-  int userId;
-  String username;
-  String nameOfUser;
-  boolean loginStatus;
+  private int userId;
+  private String username;
+  private String nameOfUser;
+  private boolean loginStatus;
+  
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getNameOfUser() {
+        return nameOfUser;
+    }
+
+    public void setNameOfUser(String nameOfUser) {
+        this.nameOfUser = nameOfUser;
+    }
+
+    public boolean getLoginStatus() {
+        return loginStatus;
+    }
+
+    public void setLoginStatus(boolean loginStatus) {
+        this.loginStatus = loginStatus;
+    } 
 }
