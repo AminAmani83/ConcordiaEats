c9c83586b813bfc3d72650fec8d6d32a785058ea
OleksandrArt <124222926+OleksandrArt@users.noreply.github.com>
2023-03-06 15:41:31 -0500

    Update Purchase.java

--- JtProject/src/main/java/ca/concordia/eats/dto/Purchase.java
+++ JtProject/src/main/java/ca/concordia/eats/dto/Purchase.java
@@ -5,5 +5,28 @@ public class Purchase {
     private int purchaseId;
     private long timeStamp;
     private float totalPrice;
- 
+  
+    public int purchaseId() {
+        return purchaseId;
+    }
+
+    public void setPurchaseId(int purchaseId) {
+        this.purchaseId = purchaseId;
+    }
+  
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+  
+    public float getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(float totalPrice) {
+        this.totalPrice = totalPrice;
+    } 
 }
