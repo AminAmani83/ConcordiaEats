a4b4753743582a29f7a54e96573ccf9b721f2126
OleksandrArt <124222926+OleksandrArt@users.noreply.github.com>
2023-03-06 16:12:04 -0500

    Update Recommendation.java

--- JtProject/src/main/java/ca/concordia/eats/dto/Recommendation.java
+++ JtProject/src/main/java/ca/concordia/eats/dto/Recommendation.java
@@ -1,4 +1,18 @@
 package ca.concordia.eats.dto;
 
 public class Recommendation {
+  
+    private List<Product> customerSearchedProducts;
+  
+    public Initialized() {
+        this.customerSearchedProducts = new ArrayList<Product>();
+    }
+      
+    public List<Product> getCustomerSearchedProducts() {
+        return new ArrayList(customerSearchedProducts);
+    }
+
+    public void setCustomerSearchedProducts(List<Product> customerSearchedProducts) {
+        this.customerSearchedProducts = (customerSearchedProducts == null) ? new ArrayList<Product>() : new ArrayList<Product>(customerSearchedProducts);
+    }
 }
