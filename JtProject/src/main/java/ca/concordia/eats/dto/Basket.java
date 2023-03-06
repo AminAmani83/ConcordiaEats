93eb9aa0332ead7ebed6a730236fdc9e7fb39d6b
OleksandrArt <124222926+OleksandrArt@users.noreply.github.com>
2023-03-06 15:12:16 -0500

    Update Basket.java

--- JtProject/src/main/java/ca/concordia/eats/dto/Basket.java
+++ JtProject/src/main/java/ca/concordia/eats/dto/Basket.java
@@ -1,4 +1,23 @@
 package ca.concordia.eats.dto;
 
 public class Basket {
+  
+    private int basketId;
+    private float totalPrice;
+  
+    public int getBasketId() {
+        return basketId;
+    }
+
+    public void setBasketId(int basketId) {
+        this.basketId = basketId;
+    }
+  
+    public float getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(float totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 }
