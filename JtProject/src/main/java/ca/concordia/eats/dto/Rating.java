6b5e369b11d416018e789349c6a15ef2f7c5cb43
OleksandrArt <124222926+OleksandrArt@users.noreply.github.com>
2023-03-06 16:24:10 -0500

    Update Rating.java

--- JtProject/src/main/java/ca/concordia/eats/dto/Rating.java
+++ JtProject/src/main/java/ca/concordia/eats/dto/Rating.java
@@ -1,4 +1,19 @@
 package ca.concordia.eats.dto;
 
 public class Rating {
+  
+    Map<Integer, Integer> customerRatings = new HashMap<Integer, Integer>();
+    private List<Product> rateableProducts;
+  
+    public Initializing() {
+        this.rateableProducts = new ArrayList<Product>();
+    }
+      
+    public List<Product> getRateableProducts() {
+        return new ArrayList(rateableProducts);
+    }
+
+    public void setRateableProducts(List<Product> rateableProducts) {
+        this.rateableProducts = (rateableProducts == null) ? new ArrayList<Product>() : new ArrayList<Product>(rateableProducts);
+    }
 }
