282dcc7466ea3c5aa85e63d22f2070b1742c6126
OleksandrArt <124222926+OleksandrArt@users.noreply.github.com>
2023-03-06 15:34:33 -0500

    Update Promotion.java

--- JtProject/src/main/java/ca/concordia/eats/dto/Promotion.java
+++ JtProject/src/main/java/ca/concordia/eats/dto/Promotion.java
@@ -1,4 +1,23 @@
 package ca.concordia.eats.dto;
 
 public class Promotion {
+  
+    private int promotionId;
+    private int promotionTypeId; 
+  
+    public int getPromotionId() {
+        return promotionId;
+    }
+
+    public void setPromotionId(int promotionId) {
+        this.promotionId = promotionId;
+    }
+    
+    public int getPromotionTypeId() {
+        return promotionTypeId;
+    }
+
+    public void setPromotionTypeId(int promotionTypeId) {
+        this.promotionTypeId = promotionTypeId;
+    }
 }
