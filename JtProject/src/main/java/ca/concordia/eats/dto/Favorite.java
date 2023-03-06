73bf12c752f0b2cd5abe38fbe51cb1b18266727a
OleksandrArt <124222926+OleksandrArt@users.noreply.github.com>
2023-03-06 16:08:36 -0500

    Update Favorite.java

--- JtProject/src/main/java/ca/concordia/eats/dto/Favorite.java
+++ JtProject/src/main/java/ca/concordia/eats/dto/Favorite.java
@@ -1,4 +1,20 @@
 package ca.concordia.eats.dto;
 
+import java.util.*;
+
 public class Favorite {
+  
+    private List<Product> customerFavoritedProducts;
+  
+    public Initialize() {
+        this.customerFavoritedProducts = new ArrayList<Product>();
+    }
+      
+    public List<Product> getCustomerFavoritedProducts() {
+        return new ArrayList(customerFavoritedProducts);
+    }
+
+    public void setCustomerFavoritedProducts(List<Product> customerFavoritedProducts) {
+        this.customerFavoritedProducts = (customerFavoritedProducts == null) ? new ArrayList<Product>() : new ArrayList<Product>(customerFavoritedProducts);
+    }
 }
