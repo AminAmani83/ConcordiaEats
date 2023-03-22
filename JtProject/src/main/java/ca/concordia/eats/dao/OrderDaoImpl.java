25f07bf14c80fd820d77e88e4d590b0ad1088b08
aminamani83 <aminamani83@gmail.com>
2023-03-22 00:15:00 -0400

    Bug Fix

--- JtProject/src/main/java/ca/concordia/eats/dao/OrderDaoImpl.java
+++ JtProject/src/main/java/ca/concordia/eats/dao/OrderDaoImpl.java
@@ -3,5 +3,5 @@ package ca.concordia.eats.dao;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class OrderDaoImpl implements UserDao {
+public class OrderDaoImpl implements OrderDao {
 }
