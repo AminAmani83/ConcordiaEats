0f5b77105c6a2f5e6df07577eea46ddc0e9b9654
Jean-Lou Gosselin <jeanlou.gosselin@gmail.com>
2023-03-25 22:57:15 -0400

    implemented UserService.java

--- JtProject/src/main/java/ca/concordia/eats/service/UserService.java
+++ JtProject/src/main/java/ca/concordia/eats/service/UserService.java
@@ -1,4 +1,15 @@
 package ca.concordia.eats.service;
 
+import ca.concordia.eats.dto.User;;
+
+import java.util.List;
+
 public interface UserService {
+    // CRUD USER
+    public List<User> getAlUsers();
+    public User getUserById(int userId);
+    public User updateUser(User user);
+    public User createUser(User user);
+    public boolean removeUser(User user);
+
 }
