bcfe9c0683e0047b3bf57d821cc810a2efd96501
Jean-Lou Gosselin <jeanlou.gosselin@gmail.com>
2023-03-28 23:25:16 -0400

    UserCredentials is now the parent class for Users

--- JtProject/src/main/java/ca/concordia/eats/dto/UserCredentials.java
+++ JtProject/src/main/java/ca/concordia/eats/dto/UserCredentials.java
@@ -1,8 +1,23 @@
 package ca.concordia.eats.dto;
 
-public class UserCredentials extends User {
+/**
+ * UserCredential is the main Class for Users, Admin, Customer.
+ */
+public class UserCredentials {
     private String username;
     private String password;
+
+    public UserCredentials() {
+    }
+
+    public UserCredentials(String username) {
+        this.username = username;
+    }
+
+    public UserCredentials(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 
     public void setUsername(String username) {
         this.username = username;
