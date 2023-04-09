8f8b9fabff6e7326a1b34ccf40d857bd08110e12
Jean-Lou Gosselin <jeanlou.gosselin@gmail.com>
2023-04-08 21:19:46 -0400

    fixed typo in docstring

--- JtProject/src/main/java/ca/concordia/eats/controller/AdminController.java
+++ JtProject/src/main/java/ca/concordia/eats/controller/AdminController.java
@@ -243,7 +243,7 @@ public class AdminController {
 
 	/**
 	 * To allow the admin to remove customers from the customer panel.
-	 * @param userCredentials
+	 * @param customerId
 	 * @return
 	 */
 	@GetMapping("/admin/customers/delete")
@@ -256,7 +256,7 @@ public class AdminController {
 	/**
 	 * The admin can update only certain information from the customer.
 	 * The admin can only do so by 'id'.
-	 * @param userCredentials
+	 * @param customerId
 	 * @param email
 	 * @param address
 	 * @param phone
