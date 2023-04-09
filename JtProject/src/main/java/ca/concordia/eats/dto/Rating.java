4687024a3110d6f2893cc7946d467df39b5c1c8c
Jean-Lou Gosselin <jeanlou.gosselin@gmail.com>
2023-04-09 10:57:30 -0400

    reintroduced the rateableProducts instance variable

--- JtProject/src/main/java/ca/concordia/eats/dto/Rating.java
+++ JtProject/src/main/java/ca/concordia/eats/dto/Rating.java
@@ -2,15 +2,22 @@ package ca.concordia.eats.dto;
 
 import java.util.*;
 
+/**
+ * Instance variables:
+ *  customerRatings maps a the rating from this customer to a productId: <productId, rating>
+ *  rateableProducts contains Products that were purchased by this customer. 
+ */
 public class Rating {
   
     Map<Integer, Integer> customerRatings = new HashMap<Integer, Integer>();        // <productId, rating>
+    Set<Product> rateableProducts = new HashSet<Product>();                         // this variable contains only past purchased Products
   
     public Rating () {
     }
 
-    public Rating(Map<Integer,Integer> customerRatings) {
+    public Rating(Map<Integer,Integer> customerRatings, Set<Product> rateableProducts) {
         this.customerRatings = customerRatings;
+        this.rateableProducts = rateableProducts;
     }
 
     public Map<Integer,Integer> getCustomerRatings() {
@@ -19,6 +26,14 @@ public class Rating {
 
     public void setCustomerRatings(Map<Integer,Integer> customerRatings) {
         this.customerRatings = (customerRatings == null) ? new HashMap<Integer,Integer>() : new HashMap<Integer,Integer>(customerRatings);
+    }
+
+    public Set<Product> getRateableProducts() {
+        return rateableProducts;
+    }
+
+    public void setRateableProducts(Set<Product> rateableProducts) {
+        this.rateableProducts = (rateableProducts == null) ? new HashSet<Product>() : new HashSet<Product>(rateableProducts);
     }
 
 }
