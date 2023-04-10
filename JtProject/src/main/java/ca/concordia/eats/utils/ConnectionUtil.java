445add81b924ce179fda9f89a680b3c5e732efff
Jean-Lou Gosselin <jeanlou.gosselin@gmail.com>
2023-04-10 15:53:02 -0400

    introduced ConnectionUtil.java and changed connection management in DAO + Controller

---
+++ JtProject/src/main/java/ca/concordia/eats/utils/ConnectionUtil.java
@@ -0,0 +1,33 @@
+package ca.concordia.eats.utils;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+public class ConnectionUtil {
+
+    private static Connection con;
+
+    public static Connection getConnection() throws IOException {
+
+        if (con==null) {
+            String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath()
+            .replaceAll("%20", " ");
+            String dbConfigPath = rootPath + "db.properties";
+    
+            FileReader reader = new FileReader(dbConfigPath);
+            Properties dbProperties = new Properties();
+            dbProperties.load(reader);
+    
+            try {
+                con = DriverManager.getConnection(dbProperties.getProperty("url"), dbProperties.getProperty("username"), dbProperties.getProperty("password"));
+            } catch (Exception e) {
+                System.out.println("Error connecting to the DB: " + e.getMessage());
+            }    
+        }
+
+        return con;
+    }
+}
