083d79a2877e6999931bc6c4a932f92640de0d1d
Jean-Lou Gosselin <jeanlou.gosselin@gmail.com>
2023-04-10 21:17:14 -0400

    added star ratings to carousel items

--- JtProject/src/main/java/ca/concordia/eats/controller/AdminController.java
+++ JtProject/src/main/java/ca/concordia/eats/controller/AdminController.java
@@ -57,16 +57,24 @@ public class AdminController {
 		} else {
 			List<Product> allProducts = productService.fetchAllProducts();
 			Customer customer = (Customer) session.getAttribute("user");
+			
 			// Temp Products TODO: use actual results from DB
-			Product bestSellerProduct = new Product(1, "Hamburger", "Delicious", "https://tmbidigitalassetsazure.blob.core.windows.net/secure/RMS/attachments/37/1200x1200/Sausage-Sliders-with-Cran-Apple-Slaw_exps48783_SD2235819D06_24_2bC_RMS.jpg", 0f, 0, false, 0f, null, null);
-			Product highestRatedProduct = new Product(2, "Chicken Soup", "Delicious", "https://www.allrecipes.com/thmb/NgpgUebR7ixeEuToPd1c1TgaQmU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/8814_HomemadeChickenSoup_SoupLovingNicole_LSH-2000-4ae7ff733c554fdab0796d15c8d1151f.jpg", 0f, 0, false, 0f, null, null);
-			Product recommendedProduct = new Product(3, "Hamburger", "Delicious", "https://tastesbetterfromscratch.com/wp-content/uploads/2017/04/Tiramisu-14.jpg", 0f, 0, false, 0f, null, null);
+			Product bestSellerProduct = new Product(1, "Hamburger", "Delicious", "https://tmbidigitalassetsazure.blob.core.windows.net/secure/RMS/attachments/37/1200x1200/Sausage-Sliders-with-Cran-Apple-Slaw_exps48783_SD2235819D06_24_2bC_RMS.jpg", 0f, 0, false, 0f, 3.0, null);
+//			bestSellerProduct.setRating(productService.calculateAvgProductRating(bestSellerProduct.getId()));
+
+			Product highestRatedProduct = new Product(2, "Chicken Soup", "Delicious", "https://www.allrecipes.com/thmb/NgpgUebR7ixeEuToPd1c1TgaQmU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/8814_HomemadeChickenSoup_SoupLovingNicole_LSH-2000-4ae7ff733c554fdab0796d15c8d1151f.jpg", 0f, 0, false, 0f, 4.5, null);
+//			highestRatedProduct.setRating(productService.calculateAvgProductRating(highestRatedProduct.getId()));
+
+			Product recommendedProduct = new Product(3, "Hamburger", "Delicious", "https://tastesbetterfromscratch.com/wp-content/uploads/2017/04/Tiramisu-14.jpg", 0f, 0, false, 0f, 3.5, null);
+//			recommendedProduct.setRating(productService.calculateAvgProductRating(recommendedProduct.getId()));
+
 			model.addAttribute("username", usernameForClass);
 			model.addAttribute("allProducts", allProducts);
 			model.addAttribute("bestSellerProduct", bestSellerProduct);
 			model.addAttribute("highestRatedProduct", highestRatedProduct);
 			model.addAttribute("recommendedProduct", recommendedProduct);
 			model.addAttribute("favoriteProducts", customer.getFavorite().getCustomerFavoritedProducts());
+
 			return "index";
 		}
 	}
