77811704c2441cae79ce4ff42db66be758f49c34
aminamani83 <aminamani83@gmail.com>
2023-04-11 02:16:15 -0400

    Adding Static Images is Now Possible

--- JtProject/src/main/java/ca/concordia/eats/ConcordiaEatsApplication.java
+++ JtProject/src/main/java/ca/concordia/eats/ConcordiaEatsApplication.java
@@ -2,12 +2,22 @@ package ca.concordia.eats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
-public class ConcordiaEatsApplication {
+public class ConcordiaEatsApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConcordiaEatsApplication.class, args);
 	}
 
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// Register resource handler for images
+		registry.addResourceHandler("/images/**").addResourceLocations("/WEB-INF/images/")
+				.setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS).cachePublic());
+	}
 }
