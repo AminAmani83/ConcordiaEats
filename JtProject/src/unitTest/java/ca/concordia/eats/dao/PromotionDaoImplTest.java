7ffc4df25ec7a2be3380e24516b13eaf8b67740e
aminamani83 <aminamani83@gmail.com>
2023-04-14 17:10:02 -0400

    Even More Unit Tests

--- JtProject/src/unitTest/java/ca/concordia/eats/dao/PromotionDaoImplTest.java
+++ JtProject/src/unitTest/java/ca/concordia/eats/dao/PromotionDaoImplTest.java
@@ -6,14 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -38,6 +37,7 @@ public class PromotionDaoImplTest {
         mockStatement = mock(PreparedStatement.class);
         mockResultSet = mock(ResultSet.class);
         when(mockConnection.prepareStatement(anyString())).thenReturn(mockStatement);
+        when(mockConnection.prepareStatement(anyString(), anyInt())).thenReturn(mockStatement);
         when(mockStatement.executeQuery()).thenReturn(mockResultSet);
         when(mockStatement.executeQuery(anyString())).thenReturn(mockResultSet);
 
@@ -65,5 +65,86 @@ public class PromotionDaoImplTest {
         assertEquals("Discount", promotion.getType());
     }
 
+    @Test
+    public void testFetchPromotionById() throws SQLException, DAOException {
+        // Given
+        int promotionId = 1;
+        when(mockResultSet.next()).thenReturn(true);
+        Promotion expectedPromotion = new Promotion();
+        expectedPromotion.setId(1);
+        expectedPromotion.setStartDate(new Date());
+        expectedPromotion.setEndDate(new Date());
+        expectedPromotion.setName("Promotion 1");
+        expectedPromotion.setType("Discount");
 
+        when(mockResultSet.getInt(1)).thenReturn(expectedPromotion.getId());
+        when(mockResultSet.getDate(2)).thenReturn(new java.sql.Date(new Date().getTime()));
+        when(mockResultSet.getDate(3)).thenReturn(new java.sql.Date(new Date().getTime()));
+        when(mockResultSet.getString(4)).thenReturn(expectedPromotion.getName());
+        when(mockResultSet.getString(5)).thenReturn(expectedPromotion.getType());
+
+        // When
+        Promotion actualPromotion = promotionDaoImpl.fetchPromotionById(promotionId);
+
+        // Then
+        assertEquals(expectedPromotion.getId(), actualPromotion.getId());
+        assertEquals(expectedPromotion.getName(), actualPromotion.getName());
+        assertEquals(expectedPromotion.getType(), actualPromotion.getType());
+    }
+
+    @Mock
+    private ResultSet mockGeneratedKeys;
+
+    @Test
+    public void testCreatePromotion() throws SQLException, DAOException {
+        // Given
+        when(mockStatement.executeUpdate()).thenReturn(1);
+        when(mockStatement.getGeneratedKeys()).thenReturn(mockGeneratedKeys);
+        when(mockGeneratedKeys.next()).thenReturn(true);
+        when(mockGeneratedKeys.getInt(1)).thenReturn(1);
+
+        Promotion promotion = new Promotion();
+        promotion.setStartDate(new Date());
+        promotion.setEndDate(new Date());
+        promotion.setName("Promotion 1");
+        promotion.setType("Discount");
+
+        // When
+        Promotion actualPromotion = promotionDaoImpl.createPromotion(promotion);
+
+        // Then
+        assertEquals(1, actualPromotion.getId());
+    }
+
+    @Test
+    public void testUpdatePromotion() throws DAOException, SQLException {
+        // Given
+        when(mockStatement.executeUpdate()).thenReturn(1);
+
+        Promotion promotion = new Promotion();
+        promotion.setId(1);
+        promotion.setStartDate(new Date());
+        promotion.setEndDate(new Date());
+        promotion.setName("Promotion 1");
+        promotion.setType("Discount");
+
+        // When
+        Promotion actualPromotion = promotionDaoImpl.updatePromotion(promotion);
+
+        // Then
+        assertEquals(promotion, actualPromotion);
+    }
+
+    @Test
+    public void testRemovePromotion() throws SQLException, DAOException {
+        // Given
+        when(mockStatement.executeUpdate()).thenReturn(1);
+        int promotionId = 1;
+
+        // When
+        boolean result = promotionDaoImpl.removePromotion(promotionId);
+
+        // Then
+        assertTrue(result);
+    }
 }
